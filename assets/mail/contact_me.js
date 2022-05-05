91e32c61470613f275915bab7b0954f90aed9137
Wilberforce Wairagu Wahome <wairaguwilberforce99@gmail.com>
2022-05-05 22:16:33 +0300

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://5e21hlgpx1.execute-api.eu-west-1.amazonaws.com/default/predict_lambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
